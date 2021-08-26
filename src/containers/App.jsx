a5e47301214f71c78d2ed5c268d10793e0e9dc78
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-08-26 10:11:17 -0500

    Added serach component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 //Components
 import Header from "../components/Header";
+import Search from "../components/Search";
 
 //styles
 import "../assets/styles/App.scss";
@@ -10,6 +11,7 @@ export default function App() {
     return (
         <div id="App">
             <Header/>
+            <Search/>
         </div>
     )
 }
