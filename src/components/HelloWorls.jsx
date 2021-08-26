61f01ca722834dbb21bc399527fa891f1255285c
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-08-25 19:16:24 -0500

    Eslint configured and structure project added

--- src/components/HelloWorls.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "../assets/styles/styles.scss";
-
-export default function HelloWorls() {
-    return (
-        <h1>Hello world!!</h1>
-    )
-}
---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Components
+import Header from "../components/Header";
+
+export default function App() {
+    return (
+        <div id="app">
+            <Header/>
+        </div>
+    )
+}
