9c6fe781021c21355a9fc031e7f7e139fcf28f0e
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2021-08-30 15:32:39 -0500

    Register page was added in the app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 //components
 import Home from "../containers/Home";
 import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Route exact path="/" component={Home} />
 			<Route exact path="/login" component={Login} />
+			<Route exact path="/register" component={Register} />
 		</BrowserRouter>
 	);
 }
